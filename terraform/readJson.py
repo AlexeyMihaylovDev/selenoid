dcc345441b947ece87d7873b943e23a23dddb912
alexey <alexeymihaylovdev@gmail.com>
2022-10-12 14:12:14 +0300

    update

--- terraform/readJson.py
+++ terraform/readJson.py
@@ -1,6 +1,6 @@
 import json
 
-with open('exam.json', 'r') as fcc_file:
+with open('output.json', 'r') as fcc_file:
     fcc_data = json.load(fcc_file)
     # print(json.dumps(fcc_data, indent=4))
     print(fcc_data['public_ip']['value'])
